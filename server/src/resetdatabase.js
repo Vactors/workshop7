107f95dfbb3ff5bb3a60b1110819e714052eac35
Vactors <aknowleskp@gmail.com>
2016-12-06 19:29:00 -0500

    databased

--- server/src/resetdatabase.js
+++ server/src/resetdatabase.js
@@ -95,6 +95,12 @@ var initialData = {
   }
 };
 
+/**
+ * Adds any desired indexes to the database.
+ */
+function addIndexes(db, cb) {
+  db.collection('feedItems').createIndex({ "contents.contents": "text" }, null, cb);
+}
 
 /**
  * Resets a collection.
@@ -132,7 +138,7 @@ function resetDatabase(db, cb) {
       // Use myself as a callback.
       resetCollection(db, collection, processNextCollection);
     } else {
-      cb();
+      addIndexes(db, cb);
     }
   }
 
